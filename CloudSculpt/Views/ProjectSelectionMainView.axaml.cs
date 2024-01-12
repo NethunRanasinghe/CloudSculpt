14f7df6e49e567daa4491c94f928bad2bb51ce1b
NethunRanasinghe <nethun223@gmail.com>
2024-01-12 21:41:20 +0530

    Changed Assets Build Mode to Avalonia Resource + Installed Avalonia Messagebox nuget package

--- CloudSculpt/Views/ProjectSelectionMainView.axaml.cs
+++ CloudSculpt/Views/ProjectSelectionMainView.axaml.cs
@@ -1,6 +1,10 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 namespace CloudSculpt.Views;
 
