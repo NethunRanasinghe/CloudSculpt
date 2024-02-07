cfb94f5a331593222396897c15b2a5bbf9718e5c
nethunranasingha <nethun223@gmail.com>
2024-02-08 01:05:27 +0530

    Need to implement proper docker exec handling. Commit to save

--- CloudSculpt/Commands/ConfigCloudInfraEditTerminalKeyDownCommand.cs
+++ CloudSculpt/Commands/ConfigCloudInfraEditTerminalKeyDownCommand.cs
@@ -1,17 +1,72 @@
-﻿using Avalonia.Controls;
+﻿using System.Threading.Tasks;
+using Avalonia.Controls;
+using CliWrap;
+using CliWrap.Buffered;
 using CloudSculpt.ViewModels;
 
 namespace CloudSculpt.Commands;
 
 public class ConfigCloudInfraEditTerminalKeyDownCommand: CommandBase
 {
-    public override void Execute(object? parameter)
+    public override async void Execute(object? parameter)
     {
         if (parameter is not TextBox e) return;
         if (e.DataContext is not ServiceElementViewModel serviceElementViewModel)
             return;
         
-        serviceElementViewModel.ConfigCloudInfraTerminalOutput += $"Command >> {e.Text}\n";
+        // Input
+        var inputValue = e.Text;
+        serviceElementViewModel.ConfigCloudInfraTerminalOutput += $"{inputValue}\n";
+
+        if (!string.IsNullOrWhiteSpace(inputValue))
+        {
+            inputValue = inputValue.Trim();
+            
+            // Execute
+            try
+            {
+                // Output
+                var outputValue = await ExecuteCommand(serviceElementViewModel, inputValue);
+                serviceElementViewModel.ConfigCloudInfraTerminalOutput += $"{outputValue}\n";
+            }
+            catch (CliWrap.Exceptions.CommandExecutionException exception)
+            {
+                if (exception.Message.Contains("not running"))
+                {
+                    var outputValue = await RunAndExecuteCommand(serviceElementViewModel, inputValue);
+                    serviceElementViewModel.ConfigCloudInfraTerminalOutput += $"{outputValue}\n";
+                }
+            }
+            
+        
+            
+        }
         e.Text = string.Empty;
+    }
+
+    private async Task<string> ExecuteCommand(ServiceElementViewModel serviceElementViewModel, string inputValue)
+    {
+        var outputValue = await Cli.Wrap("docker")
+            .WithArguments(args => args
+                .Add("exec")
+                .Add("-d")
+                .Add(serviceElementViewModel.ContainerId)
+                .Add(inputValue))
+            .ExecuteBufferedAsync();
+
+        return outputValue.StandardOutput;
+    }
+    
+    private async Task<string> RunAndExecuteCommand(ServiceElementViewModel serviceElementViewModel, string inputValue)
+    {
+        var outputValue = await Cli.Wrap("docker")
+            .WithArguments(args => args
+                .Add("run")
+                .Add("-d")
+                .Add(serviceElementViewModel.ContainerId)
+                .Add(inputValue))
+            .ExecuteBufferedAsync();
+
+        return outputValue.StandardOutput;
     }
 }
--- CloudSculpt/Commands/ServiceElementCanvasRemoveCommand.cs
+++ CloudSculpt/Commands/ServiceElementCanvasRemoveCommand.cs
@@ -5,9 +5,19 @@ namespace CloudSculpt.Commands;
 
 public class ServiceElementCanvasRemoveCommand (ServiceElementViewModel element) : CommandBase
 {
-    public override void Execute(object? parameter)
+    public override async void Execute(object? parameter)
     {
         var elementIndex = element.ElementIndex;
+        var containerId = element.ContainerId;
+        
+        // Remove Container
+        if (!string.IsNullOrWhiteSpace(containerId))
+        {
+            var dockerHelper = new HelperClasses.Docker();
+            await dockerHelper.StopContainer(containerId);
+            await dockerHelper.RemoveContainer(containerId);
+        }
+        
         EventAggregator.Instance.Publish(new RemoveServiceElementEvent(elementIndex));
     }
 }
